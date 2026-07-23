58ff86b428a3448e722433bb668a05e8206b1cd0
Przemek <przemo246@gmail.com>
2026-07-23 09:07:17 +0200

    feat: update ESLint configuration with tailwind-canonical-classes plugin and rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import tailwindCanonicalClasses from "eslint-plugin-tailwind-canonical-classes";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +12,19 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      "tailwind-canonical-classes": tailwindCanonicalClasses,
+    },
+    rules: {
+      "tailwind-canonical-classes/tailwind-canonical-classes": [
+        "warn",
+        {
+          cssPath: "./core/style/index.css",
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
