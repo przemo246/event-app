579d010a31331b7c76f5a8f1fdf61fab4a4f7b0e
Przemek <przemo246@gmail.com>
2026-08-27 19:24:46 +0200

    refactor(nav-bar): simplify NavBarHeader component by removing unused shadow logic

--- shared/nav-bar/presentation/nav-bar-header.tsx
+++ shared/nav-bar/presentation/nav-bar-header.tsx
@@ -1,9 +1,6 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { cn } from "@/libs/react-kit/cn";
-
-const SHADOW_PATHS = ["/login", "/register", "/account/profile", "/events/new"];
 
 type NavBarHeaderProps = {
   children: React.ReactNode;
@@ -12,14 +9,5 @@ type NavBarHeaderProps = {
 export const NavBarHeader = ({ children }: NavBarHeaderProps) => {
   const pathname = usePathname();
 
-  return (
-    <header
-      className={cn(
-        "bg-background",
-        SHADOW_PATHS.includes(pathname) && "shadow-md",
-      )}
-    >
-      {children}
-    </header>
-  );
+  return <header className="bg-background shadow-md">{children}</header>;
 };
