22b763f6d04d25608cee6f858d1eb737d08eb728
Przemek <przemo246@gmail.com>
2026-08-31 20:47:13 +0200

    Revert "refactor(event-creation): move page title and layout into Main"
    This reverts commit ae3d9e3ee5c1c2a2c5c0c0c0c0c0c0c0c0c0c0c0.

--- core/modules/event-creation.tsx
+++ core/modules/event-creation.tsx
@@ -1,10 +1,15 @@
+import { Suspense } from "react";
+
+import { Spinner } from "@/libs/ui/spinner";
 import { Provider } from "@/modules/event-creation/presentation/context";
 import { Main } from "@/modules/event-creation/presentation/main";
 
 export const Module = () => {
   return (
-    <Provider>
-      <Main />
-    </Provider>
+    <Suspense fallback={<Spinner />}>
+      <Provider>
+        <Main />
+      </Provider>
+    </Suspense>
   );
 };
