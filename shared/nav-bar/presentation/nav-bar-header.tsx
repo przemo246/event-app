032e725e5d82a1401bda77fc65d63563d34fff60
Przemek <przemo246@gmail.com>
2026-09-01 21:41:51 +0200

    refactor(nav-bar-header): remove unused usePathname import

--- shared/nav-bar/presentation/nav-bar-header.tsx
+++ shared/nav-bar/presentation/nav-bar-header.tsx
@@ -1,13 +1,9 @@
 "use client";
 
-import { usePathname } from "next/navigation";
-
 type NavBarHeaderProps = {
   children: React.ReactNode;
 };
 
 export const NavBarHeader = ({ children }: NavBarHeaderProps) => {
-  const pathname = usePathname();
-
   return <header className="bg-background shadow-md">{children}</header>;
 };
